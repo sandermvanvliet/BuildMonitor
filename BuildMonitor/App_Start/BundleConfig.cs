ee798f271863565b3c82d3758b0ba0065b6983f5
sandermvanvliet <sander@codenizer.nl>
2015-09-03 07:40:19 +0200

    Got the MVC basics running

---
+++ BuildMonitor/App_Start/BundleConfig.cs
@@ -0,0 +1,12 @@
+using System.Web.Optimization;
+
+namespace BuildMonitor.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            
+        }
+    }
+}
