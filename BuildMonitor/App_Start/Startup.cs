77a6cde0c1737c893409485f9fdb87e08de95dce
sandermvanvliet <sander@codenizer.nl>
2015-09-04 07:05:50 +0200

    Added signalR and knockout

---
+++ BuildMonitor/App_Start/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace BuildMonitor.App_Start
+{
+    public class Startup
+    {
+        public void Configure(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
