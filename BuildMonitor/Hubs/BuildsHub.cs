5c67010aa558c2f4d90c5ecc6371c355d22b1f7d
sandermvanvliet <sander@codenizer.nl>
2015-09-04 17:13:34 +0200

    Added in builds hub

---
+++ BuildMonitor/Hubs/BuildsHub.cs
@@ -0,0 +1,13 @@
+﻿using BuildMonitor.Models;
+using Microsoft.AspNet.SignalR;
+
+namespace BuildMonitor.Hubs
+{
+    public class BuildsHub : Hub
+    {
+        public void BuildStatusChanged(Build build)
+        {
+            Clients.All.broadcastMessage(build);
+        }
+    }
+}
---
+++ BuildMonitor/Models/Build.cs
@@ -0,0 +1,8 @@
+namespace BuildMonitor.Models
+{
+    public class Build
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+}
